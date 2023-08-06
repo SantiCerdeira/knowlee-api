c8c489efd1fbf884056508a01a625434a5764c29
Santiago Cerdeira <santiagocerdeira808@gmail.com>
2023-08-06 18:50:36 -0300

    fixing cookie

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -72,7 +72,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: "none",
       secure: true,
     });
 
