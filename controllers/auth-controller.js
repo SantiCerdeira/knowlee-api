24458a7263f6b23d0c07521288b5a56aea25f33e
Santiago Cerdeira <santiagocerdeira808@gmail.com>
2023-08-06 23:12:03 -0300

    fixing logout

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -74,9 +74,10 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       maxAge: 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "Lax",
+      // sameSite: "None",
       secure: true,
-      // domain: '.onrender.com',
+      domain: '.onrender.com',
     });
 
     return res.status(200).json({ token });
